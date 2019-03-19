0a5ee5f1d6e7933262837f1fb2bc5e93d4c819ec
rates <48226536+rates@users.noreply.github.com>
2019-03-19 23:23:46 +0300

    -

---
+++ precache-manifest.c3e6e22193abdae90ae2ad94f94fb82b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e499bc42484722f5f54",
+    "url": "js/2d22c0ff.8c0e2434.js"
+  },
+  {
+    "revision": "4b79a024996f8f00b3ac",
+    "url": "js/4b47640d.0b855adf.js"
+  },
+  {
+    "revision": "b372e3fc45749797f9ff",
+    "url": "css/5fb8342e.4f0684cd.css"
+  },
+  {
+    "revision": "b372e3fc45749797f9ff",
+    "url": "js/5fb8342e.5e1e0ac2.js"
+  },
+  {
+    "revision": "6b7cd357731ca11df34f",
+    "url": "css/app.d3153500.css"
+  },
+  {
+    "revision": "6b7cd357731ca11df34f",
+    "url": "js/app.1a47b59e.js"
+  },
+  {
+    "revision": "cd7ae9075a0e6e692f4c",
+    "url": "js/vendor.57242e10.js"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8a.woff"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.bafb105b.woff"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.a1471d1d.woff"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.de8b7431.woff"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.cf6613d1.woff"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.8c2ade50.woff"
+  },
+  {
+    "revision": "bca3a1873ac988faff0817eca96b2d86",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.bca3a187.woff2"
+  },
+  {
+    "revision": "9219a80f0478e0bfdee5f4c753ce8535",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.9219a80f.woff"
+  },
+  {
+    "revision": "e8e3fc37eb1d5659920d858ea2c70937",
+    "url": "statics/icons/192x192.png"
+  },
+  {
+    "revision": "3f5ec1143bce0fb96c3fcc1494e9b821",
+    "url": "index.html"
+  }
+];
