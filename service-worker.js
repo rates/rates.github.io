6487bdd30bd5a9771957a567c9275a6880767531
rates <48226536+rates@users.noreply.github.com>
2019-04-04 12:08:43 +0300

    -

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,11 @@
-importScripts("OneSignalSDKWorker.js", "OneSignalSDKUpdaterWorker.js","https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
  * is picked up by the build system ONLY if
  * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
+importScripts("OneSignalSDKWorker.js", "OneSignalSDKUpdaterWorker.js");
 self.addEventListener('fetch', function(event) {
 });
 
