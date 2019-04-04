a90e2c4a7b299c4fced919864b644e2f4c48a256
rates <48226536+rates@users.noreply.github.com>
2019-04-04 11:54:50 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("OneSignalSDKWorker.js", "OneSignalSDKWorker.js","https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("OneSignalSDKWorker.js", "OneSignalSDKUpdaterWorker.js","https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
