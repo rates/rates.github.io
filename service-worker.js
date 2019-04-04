88b0ddcfec441126d4253dfd0b7f602790ecac26
rates <48226536+rates@users.noreply.github.com>
2019-04-04 11:32:50 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,3 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
-importScripts(
-  "precache-manifest.81ada5e88ccab383b19381023689578b.js"
-);
-
-workbox.core.setCacheNameDetails({prefix: "rates"});
-
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
