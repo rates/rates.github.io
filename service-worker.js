e9c3b314dd0ba1ebfb73651f402d1ff719776435
rates <48226536+rates@users.noreply.github.com>
2019-04-04 11:14:16 +0300

    -

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.47cfce094f0db64305c4820d8d0a582c.js"
+  "precache-manifest.81ada5e88ccab383b19381023689578b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "rates"});
