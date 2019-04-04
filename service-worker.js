111e52a0d7a735980bb5870d474e73172bd1253e
rates <48226536+rates@users.noreply.github.com>
2019-04-04 11:51:43 +0300

    -

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,10 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
+importScripts("OneSignalSDKWorker.js", "OneSignalSDKWorker.js","https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/*
+ * This file (which will be your service worker)
+ * is picked up by the build system ONLY if
+ * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
+self.addEventListener('fetch', function(event) {
+});
 
