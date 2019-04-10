af45446de640bbed015947fb0006041b01955ae8
rates <48226536+rates@users.noreply.github.com>
2019-04-11 01:29:15 +0300

    -

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.9ae6ec7eaa575a11795bf8140a27008e.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.0bce5507328c2ae952720852f61a0569.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
