69409377651a94a9155749608d797d99b11a26b5
rates <48226536+rates@users.noreply.github.com>
2019-05-12 22:27:19 +0300

    -

--- service-worker.js
+++ service-worker.js
@@ -5,3 +5,5 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
  * is picked up by the build system ONLY if
  * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
+
+self.addEventListener('fetch', function(event) {});
